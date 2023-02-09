b5ffb254a1017025fa39da497f74baca8ae46a4e
Hergelink <enver.hergelink@gmail.com>
2023-02-09 13:40:03 +0300

    a function to handle scrolltotop and tooglemodal

--- client/src/components/MobileModal.jsx
+++ client/src/components/MobileModal.jsx
@@ -2,25 +2,39 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import style from '../styles/Header.module.css';
 
-export default function MobileModal({toggleMobileMenu}) {
+export default function MobileModal({ toggleMobileMenu }) {
+  const handleScroll = () => {
+    const element = document.querySelector('header');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const handleClick = () => {
+    toggleMobileMenu();
+    handleScroll();
+  };
+
   return (
     <div id={style.mobileModal}>
       <div id={style.miniHeader}>
-        <Link to='/' id={style.siteLogo} onClick={toggleMobileMenu}>StoryTime</Link>
-        <div id={style.mobileMenuBtn} onClick={toggleMobileMenu}>
+        <Link to='/' id={style.siteLogo} onClick={handleClick}>
+          StoryTime
+        </Link>
+        <div id={style.mobileMenuBtn} onClick={handleClick}>
           <span className={style.hamburger}></span>
           <span className={style.hamburger}></span>
           <span className={style.hamburger}></span>
         </div>
       </div>
       <nav id={style.mobileNav}>
-        <Link to='/' className={style.mobileLinks} onClick={toggleMobileMenu}>
+        <Link to='/' className={style.mobileLinks} onClick={handleClick}>
           Home
         </Link>
-        <Link to='/create' className={style.mobileLinks} onClick={toggleMobileMenu}>
+        <Link to='/create' className={style.mobileLinks} onClick={handleClick}>
           + Create
         </Link>
-        <Link to='/login' id={style.mobileLoginBtn} onClick={toggleMobileMenu}>
+        <Link to='/login' id={style.mobileLoginBtn} onClick={handleClick}>
           Login
         </Link>
       </nav>
